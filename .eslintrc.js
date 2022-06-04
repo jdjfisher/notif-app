7a2275a20222dd0b703b681fd9c6c09f179f3a21
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2022-06-04 11:32:50 +0100

    Enforce prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': 'error',
   },
 };

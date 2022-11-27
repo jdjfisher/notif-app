bd91abe689b160bd8b509e5d68048bc64384ea60
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2022-11-27 10:57:48 +0000

    Delay load until authenticated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import * as Sentry from 'sentry-expo';
 import Constants from 'expo-constants';
@@ -9,11 +9,7 @@ import localizedFormat from 'dayjs/plugin/localizedFormat';
 
 import usePushNotifications from './hooks/usePushNotifications';
 import useCachedResources from './hooks/useCachedResources';
-import ServerBanner from './components/ServerBanner';
-import Navigation from './navigation';
-import { useProfileStore } from './state/profileStore';
-import NotifApi from './lib/api/bindings';
-import { getPushToken } from './lib/helpers';
+import Body from './components/layout/Body';
 
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
@@ -29,26 +25,13 @@ export default function App() {
 
   usePushNotifications();
 
-  const bearerToken = useProfileStore((state) => state.bearerToken);
-
-  useEffect(() => {
-    if (bearerToken) {
-      return;
-    }
-
-    getPushToken().then((token) => {
-      NotifApi.register.apply(token);
-    });
-  }, []);
-
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
         <StatusBar />
-        <Navigation />
-        <ServerBanner />
+        <Body />
       </SafeAreaProvider>
     );
   }

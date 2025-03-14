00c17768d8bf847aec007d0e4343dd17f3adde29
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2025-03-14 21:24:46 +0000

    Updated eslint expo config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
+// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-  },
+  extends: 'expo',
+  ignorePatterns: ['/dist/*'],
 };
